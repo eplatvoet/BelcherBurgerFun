17a0a068067590089acaac3abf12fb5e226a8bed
Erin Platvoet <erin.didomenico520@gmail.com>
2020-08-29 08:51:05 -0400

    burger db created & 6 added

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    eaten BOOLEAN DEFAULT false
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, eaten) VALUES ("Bacon Bleu Burger", true);
+INSERT INTO burgers (name, eaten) VALUES ("Buffalo Burger", true);
+INSERT INTO burgers (name, eaten) VALUES ("Jersey Burger", true);
+INSERT INTO burgers (name) VALUES ("Beyond Burger");
+INSERT INTO burgers (name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Peppercorn Burger");
