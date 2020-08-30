a9cda5da8b78f110c757ba763cf876163566fe26
Erin Platvoet <erin.didomenico520@gmail.com>
2020-08-30 17:28:30 -0400

    tables updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
-    eaten BOOLEAN DEFAULT false
+    cook BOOLEAN DEFAULT false
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name, eaten) VALUES ("Bacon Bleu Burger", true);
-INSERT INTO burgers (name, eaten) VALUES ("Buffalo Burger", true);
-INSERT INTO burgers (name, eaten) VALUES ("Jersey Burger", true);
+INSERT INTO burgers (name, cook) VALUES ("Bacon Bleu Burger", true);
+INSERT INTO burgers (name, cook) VALUES ("Buffalo Burger", true);
+INSERT INTO burgers (name, cook) VALUES ("Jersey Burger", true);
 INSERT INTO burgers (name) VALUES ("Beyond Burger");
 INSERT INTO burgers (name) VALUES ("Bacon Cheeseburger");
 INSERT INTO burgers (name) VALUES ("Peppercorn Burger");
